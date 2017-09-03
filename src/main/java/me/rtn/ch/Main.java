cbd450b1a45fc3003cbdb54a87409d469e9111ab
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-03 23:21:26 +0100

    registering listeners

--- src/main/java/me/rtn/ch/Main.java
+++ src/main/java/me/rtn/ch/Main.java
@@ -1,6 +1,9 @@
 package me.rtn.ch;
 
+import me.rtn.ch.events.PlayerJoin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.stream.Stream;
 
 public final class Main extends JavaPlugin {
 
@@ -13,6 +16,10 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         instance = this;
+
+        Stream.of(
+                new PlayerJoin()
+        ).forEach(listener -> getServer().getPluginManager().registerEvents(listener, this));
     }
 
     @Override
