9af7b90bfc548eed1e4156bd0c2669d1a3ee12a6
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-04 19:27:34 +0100

    i forgot i had something to do this, lets try it.

---
+++ src/main/java/me/rtn/ch/commands/TitleStringReplacementTester.java
@@ -0,0 +1,30 @@
+package me.rtn.ch.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/*
+ * mc 
+ * Created by George at 7:27 PM on 04-Sep-17  
+ * Copyright (C) 2017 RapidTheNerd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+public class TitleStringReplacementTester implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return true;
+    }
+}
