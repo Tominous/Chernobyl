1a58d62d642875990186deac853abd8de64fb4e1
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-05 01:34:07 +0100

    i hate python sometimes

--- Yamlson/__init__.py
+++ Yamlson/__init__.py
@@ -1,2 +1,38 @@
-import yaml, json, sys;
-sys.stdout.write(json.dumps(yaml.load))
+from __future__ import print_function
+
+import argparse
+import datetime
+import dateutil.parser
+import io
+import json
+import os.path
+import re
+import string
+import sys
+import pytoml
+import yaml
+
+
+__version__ = '0.7.0'
+
+FORMATS = ['json', 'toml', 'yaml']
+if hasattr(json, 'JSONDecodeError'):
+    JSONDecodeError = json.JSONDecodeError
+else:
+    JSONDecodeError = ValueError
+
+
+def filename2format(filename):
+    possible_format = '(' + '|'.join(FORMATS) + ')'
+    match = re.search('^' + possible_format + '2' + possible_format, filename)
+    if match:
+        from_, to = match.groups()
+        return True, from_, to
+    else:
+        return False, None, None
+
+
+def json_serialize(obj):
+    if isinstance(obj, datetime.datetime):
+        return obj.isoformat()
+    raise TypeError("{0} is not JSON-serializable".format(repr(obj)))
